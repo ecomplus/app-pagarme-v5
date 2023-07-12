ddffe70bd5f7bfed1788e2b4ab5cf93559cbb3fa
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-07-12 17:04:37 -0300

    chore(firestore): functions to handle firestore

---
+++ functions/lib/firestore/utils.js
@@ -0,0 +1,22 @@
+const getDocFirestore = async (collection, documentId) => {
+  const documentSnapshot = await collection.doc(documentId).get()
+  let data
+  if (documentSnapshot) {
+    data = documentSnapshot.data()
+  }
+  return data
+}
+
+const updateDocFirestore = async (collection, documentId, body) => {
+  const updatedAt = new Date().toISOString()
+  body.updatedAt = updatedAt
+
+  await collection.doc(documentId)
+    .set(body, { merge: true })
+    .catch(console.error)
+}
+
+module.exports = {
+  getDocFirestore,
+  updateDocFirestore
+}
